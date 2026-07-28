a93cab351904dacd48fa76b250540bbb190aea27
Yoon975 <yungbin8@gmail.com>
2026-07-28 14:49:37 +0900

    지도 UI를 page로 분리하고 KakaoMap은 지도만 담당하도록 변경

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import KakaoMap from "./kakaoMap";
+import MapControls from "./MapControls";
+import GridInfoCard from "./GridInfoCard";
 
 // 담당: 지도표시팀
 
 export default function MapPage() {
-  return <KakaoMap />;
+  return (
+    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+      <KakaoMap />
+      <MapControls />
+      <GridInfoCard />
+    </div>
+  );
 }
